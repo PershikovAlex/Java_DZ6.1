125d3c9475602dd7e8597abe971b8d70647c3f93
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-04-18 16:20:55 +0500

    Add Test

---
+++ src/test/java/RestServicesTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.services.RestService;
+
+public class RestServicesTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 10000, 3000, 20000",
+            "2, 100000, 60000, 150000"
+    })
+    public void testRestService(int expected, int income, int expences, int threshold){
+
+        RestService service = new RestService();
+        int actual = service.calculate(income, expences, threshold);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+}
